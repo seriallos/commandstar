9b908139e39f4cb111456d2fec01860dee9bc487
Dave Hendler <dhendler@turbine.com>
2013-12-20 21:08:08 -0500

    Uses multiple threads to run coffeelint more quickly

--- build/lint.sh
+++ build/lint.sh
@@ -4,6 +4,7 @@ echo "Running coffeelint..."
 
 find . -name "*.coffee" -not -path "./node_modules/*" \
   | xargs -I % \
+    -P 4 \
     ./node_modules/coffeelint/bin/coffeelint \
     --quiet \
     %
