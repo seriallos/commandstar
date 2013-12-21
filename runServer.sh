d90e82837197c9ac70173eef3781534cf24b2e2f
Dave Hendler <dhendler@turbine.com>
2013-12-21 01:10:31 -0500

    No longer installs dev dependencies by default

--- runServer.sh
+++ runServer.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # make sure node_modules are up to date
-npm install
+# don't install dev dependencies by default
+npm install --production
 
 # run the server using the local coffee-script
 ./node_modules/coffee-script/bin/coffee server.coffee
